dc0b914a761c583e061df70bb2cf64332db784df
Laiot <carmelo.sarta.main@gmail.com>
2022-12-05 16:56:17 +0100

    Prettify 1

--- src/day5.rs
+++ src/day5.rs
@@ -7,18 +7,8 @@ pub struct Step {
     final_pos: usize
 }
 
-#[aoc_generator(day5, part1)]
-pub fn part1_gen(input: &str) -> String {
-    input.to_string()
-}
-
-#[aoc_generator(day5, part2)]
-pub fn part2_gen(input: &str) -> String {
-    input.to_string()
-}
-
-#[aoc(day5, part1)]
-pub fn part1(input: &str) -> String {
+#[aoc_generator(day5)]
+pub fn part1_gen(input: &str) -> (Vec<Vec<char>>, Vec<Step>) {
     let mut lines_iter_own = input.lines().peekable();
     let lines_iter = lines_iter_own.by_ref();
     let mut stacks: Vec<Vec<char>> = vec![Vec::new(); lines_iter.peek().unwrap().len()/4 + 1];
@@ -54,10 +44,15 @@ pub fn part1(input: &str) -> String {
             final_pos: *step_params.get(2).unwrap()});
     }
 
-    
+    (stacks, steps)
+}
+
+#[aoc(day5, part1)]
+pub fn part1(input: &(Vec<Vec<char>>, Vec<Step>)) -> String {
+    let mut stacks = input.0.clone();
     let mut res = String::new();
-    for step in steps {
-        for substep in 0..step.num_of_items {
+    for step in &input.1 {
+        for _ in 0..step.num_of_items {
             'stackloop: for stack in stacks.clone().get(step.init_pos - 1) {
                 for item in stack.iter().clone().rev() {
                     if !item.is_whitespace() {
@@ -78,47 +73,12 @@ pub fn part1(input: &str) -> String {
 }
 
 #[aoc(day5, part2)]
-pub fn part2(input: &str) -> String {
+pub fn part2(input: &(Vec<Vec<char>>, Vec<Step>)) -> String {
+    let mut stacks = input.0.clone();
 
-    let mut lines_iter_own = input.lines().peekable();
-    let lines_iter = lines_iter_own.by_ref();
-    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); lines_iter.peek().unwrap().len()/4 + 1];
-    let mut steps: Vec<Step> = Vec::new();
-    let mut line_counter: usize = 0;
-
-    while !lines_iter.peek().unwrap().contains('1'){
-        for (ind, item) in lines_iter.next().unwrap().chars().enumerate() {
-            if ind != 0 && (ind - 1) % 4 == 0 {
-                stacks[ind / 4].push(item);
-            }
-        }
-        line_counter += 1;
-    }
-
-    for i in 0..stacks.len(){
-        stacks[i].reverse();
-        stacks[i].retain(|&x| x != ' ');
-    }
-
-    let empty1 = lines_iter.next();
-    let empty2 = lines_iter.next();
-
-    let re = Regex::new(r"\d+").unwrap();
-
-    for line in lines_iter {
-        let step_params: Vec<usize> = re.find_iter(line)
-            .filter_map(|digits| digits.as_str().parse().ok())
-            .collect();
-        
-        steps.push(Step {num_of_items: *step_params.get(0).unwrap(), 
-            init_pos: *step_params.get(1).unwrap(), 
-            final_pos: *step_params.get(2).unwrap()});
-    }
-
-    
     let mut res = String::new();
-    for step in steps {
-        'stackloop: for stack in stacks.clone().get(step.init_pos - 1) {
+    for step in &input.1 {
+        'stackloop: for stack in input.0.clone().get(step.init_pos - 1) {
             for item in stack.iter().clone().rev() {
                 if !item.is_whitespace() {
                     let mut pops_vec: Vec<char> = Vec::new();
