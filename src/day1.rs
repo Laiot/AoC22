f29fa5796d2bbcd7ee83f7a3bff4940c0e96fb7c
Laiot <carmelo.sarta.main@gmail.com>
2022-12-06 12:16:40 +0100

    Prettify day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,16 +1,6 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-
-fn get_index_of_lowest(values: &[u32]) -> usize {
-    let mut lowest = u32::MAX;
-    let mut index = 0;
-    for i in 0..values.len() {
-        if values[i] < lowest {
-            lowest = values[i];
-            index = i;
-        }
-    }
-    index
-}
+use std::cmp::{Reverse, self};
+use std::collections::BinaryHeap;
 
 #[aoc_generator(day1)]
 pub fn day1_gen(input: &str) -> Vec<u32> {
@@ -22,14 +12,6 @@ pub fn day1_gen(input: &str) -> Vec<u32> {
         }
     });
     vec.push(last); 
-
-    // .fold(vec![0], |mut a, x|{
-    //     match x.parse::<u32>() {
-    //         Some(v) => {*a.last_mut().unwrap() += v;}
-    //         None => {a.push(0);}
-    //     };
-    //     a
-    // })
     vec
 }
 
@@ -40,13 +22,15 @@ pub fn part1(calories: &Vec<u32>) -> u32 {
 
 #[aoc(day1, part2)]
 pub fn part2(calories: &Vec<u32>) -> u32 {
-    let mut result = vec![u32::MIN; 3];
-    for cal in calories {
-        let index = get_index_of_lowest(&result);
-        if cal > &result[index] {
-            result[index] = *cal;
+    calories.iter().fold(BinaryHeap::new(), |mut h, e|{
+        if h.len() < 3 {
+            h.push(Reverse(*e))
+        } else {
+            match h.pop() {
+                Some(Reverse(x)) => {h.push(Reverse(cmp::max(x, *e)))}
+                _ => {}
+            }
         }
-    }
-
-    result.iter().sum()
+        h
+     }).iter().map(|x| x.0).sum()
 }
