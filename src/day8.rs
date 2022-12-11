72a0ffaf51a9603121cecb5e032c19d7acfca922
Laiot <carmelo.sarta.main@gmail.com>
2022-12-11 15:47:44 +0100

    Fixed part 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[derive(Debug, Copy, Clone)]
@@ -24,7 +26,7 @@ pub fn day8_gen(input: &str) -> Vec<Tree> {
 #[aoc(day8, part1)]
 pub fn part1(trees: &Vec<Tree>) -> usize {
 
-    let row_len = 5;
+    let row_len = 99;
     let edge_counter = (row_len * 4) - 4;
     let mut visible_trees: Vec<Tree> = Vec::new();
     let mut left: bool;
@@ -67,106 +69,36 @@ pub fn part1(trees: &Vec<Tree>) -> usize {
 }
 
 #[aoc(day8, part2)]
-pub fn part2(trees: &Vec<Tree>) -> u32 {
+pub fn part2(trees: &Vec<Tree>) -> usize {
     let mut res = 0;
-    let row_len = 5;
-    let mut visible_trees: Vec<Tree> = Vec::new();
-    let mut left: bool;
-    let mut right: bool;
-    let mut up: bool;
-    let mut down: bool;
+    let row_len = 99;
 
     for tree in trees {
         if tree.pos_x > 0 && tree.pos_x < row_len - 1 && tree.pos_y > 0 && tree.pos_y < row_len - 1 {
-            left = true;
-            right = true;
-            up = true;
-            down = true;
+            let (mut up, mut down, mut left, mut right) = (tree.pos_y, 98 - tree.pos_y, tree.pos_x, 98 - tree.pos_x);
             for test_tree in trees {
                 if test_tree.pos_x == tree.pos_x && test_tree.pos_y > tree.pos_y {
                     if test_tree.height >= tree.height {
-                        down = false;
+                        down = min(down, test_tree.pos_y - tree.pos_y);
                     }
                 } else if test_tree.pos_x == tree.pos_x && test_tree.pos_y < tree.pos_y {
                     if test_tree.height >= tree.height {
-                        up = false;
+                        up = min(up, tree.pos_y - test_tree.pos_y);
                     }
                 } else if test_tree.pos_y == tree.pos_y && test_tree.pos_x < tree.pos_x {
                     if test_tree.height >= tree.height {
-                        right = false;
+                        right = min(right, tree.pos_x - test_tree.pos_x);
                     }
                 } else if test_tree.pos_y == tree.pos_y && test_tree.pos_x > tree.pos_x {
                     if test_tree.height >= tree.height {
-                        left = false;
+                        left = min(left, test_tree.pos_x - tree.pos_x);
                     }
                 }
             }
-            if down || up || right || left {
-                visible_trees.push(*tree);
+            let new_max = down * up * right * left;
+            if new_max > res {
+                res = new_max;
             }
-        }
-    }
-
-    for visible_tree in visible_trees {
-        let mut count = 0;
-        let mut top = 1;
-        for a in (0..visible_tree.pos_x).rev() {
-            if trees.iter().find(|tree| tree.pos_y == visible_tree.pos_y && tree.pos_x == a && tree.height < visible_tree.height).is_some(){
-                count += 1;
-            } else {
-                if count == 0 {count = 1}
-                top *= count;
-                count = 0;
-                break;
-            };
-        }
-        if count == 0 {count = 1}
-        top *= count;
-        count = 0;
-        for b in visible_tree.pos_x..5 {
-            if trees.iter().find(|tree| tree.pos_y == visible_tree.pos_y && tree.pos_x == b && tree.height < visible_tree.height).is_some(){
-                count += 1;
-            } else {
-                if count == 0 {count = 1}
-
-                top *= count;
-                count = 0;
-                break;
-            };
-        }
-        if count == 0 {count = 1}
-        top *= count;
-        count = 0;
-        for c in (0..visible_tree.pos_y).rev() {
-            if trees.iter().find(|tree| tree.pos_y == c && tree.pos_x == visible_tree.pos_x && tree.height < visible_tree.height).is_some(){
-                count += 1;
-            } else {
-                if count == 0 {count = 1}
-
-                top *= count;
-                count = 0;
-                break;
-            };
-        }
-        if count == 0 {count = 1}
-        top *= count;
-        count = 0;
-        for d in visible_tree.pos_y..5 {
-            if trees.iter().find(|tree| tree.pos_y == d && tree.pos_x == visible_tree.pos_x && tree.height < visible_tree.height).is_some(){
-                count += 1;
-            } else {
-                if count == 0 {count = 1}
-
-                top *= count;
-                count = 0;
-                break;
-            };
-        }
-        if count == 0 {count = 1}
-        top *= count;
-
-        if res < top {
-            res = top;
         }
     }
 
