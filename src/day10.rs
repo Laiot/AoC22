22523f05c55f99d5f58b2cd8e641038e72b2f181
Laiot <carmelo.sarta.main@gmail.com>
2022-12-14 06:05:28 +0100

    Pushed day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -4,6 +4,10 @@ pub fn check_cycle_count(cycle_count: i32) -> bool {
     cycle_count == 20 || cycle_count == 60 || cycle_count == 100 || cycle_count == 140 || cycle_count == 180 || cycle_count == 220 
 }
 
+pub fn check_second_cycle_count(cycle_count: i32) -> bool {
+    cycle_count == 40 || cycle_count == 80 || cycle_count == 120 || cycle_count == 160 || cycle_count == 200 || cycle_count == 240 
+}
+
 #[aoc(day10, part1)]
 pub fn part1(input: &str) -> i32 {
     let mut signal = 1;
@@ -21,13 +25,11 @@ pub fn part1(input: &str) -> i32 {
                 if check_cycle_count(cycle_count) {
                     saved_signals.push(signal * cycle_count);
                 }
-                println!("{}", cycle_count*signal);
-                signal += args;
                 cycle_count += 1;
                 if check_cycle_count(cycle_count) {
                     saved_signals.push(signal * cycle_count);
                 }
-                println!("{}", cycle_count*signal);
+                signal += args;
 
             },
             _ => {
@@ -35,8 +37,6 @@ pub fn part1(input: &str) -> i32 {
                 if check_cycle_count(cycle_count) {
                     saved_signals.push(signal * cycle_count);
                 }
-                println!("{}", cycle_count*signal);
-
             }
         }
     }
@@ -46,8 +46,54 @@ pub fn part1(input: &str) -> i32 {
 }
 
 #[aoc(day10, part2)]
-pub fn part2(motions: &str) -> usize {
+pub fn part2(input: &str) -> usize {
     let res = 0;
+    let mut cycle_count = 0;
+    let mut cursor: i32 = 1;
+
+    for command in input.lines() {
+        let command_split: Vec<&str> = command.split(" ").collect();
+        let cmd = command_split[0];
+        
+        match cmd {
+            "addx" => {
+                let args = command_split[1].parse::<i32>().unwrap();
+                if (cursor - cycle_count).abs() <= 1 {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+                cycle_count += 1;
+                if check_second_cycle_count(cycle_count) {
+                    print!("\n");
+                    cycle_count = 0;
+                }
+                if (cursor - cycle_count).abs() <= 1 {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+                cycle_count += 1;
+                if check_second_cycle_count(cycle_count) {
+                    print!("\n");
+                    cycle_count = 0;
+                }
+                cursor += args;
+            },
+            _ => {
+                if (cursor - cycle_count).abs() <= 1 {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+                cycle_count += 1;
+                if check_second_cycle_count(cycle_count) {
+                    print!("\n");
+                    cycle_count = 0;
+                }
+            }
+        }
+    }
 
     res
 }
